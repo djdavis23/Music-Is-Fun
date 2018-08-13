a7224b9bea85e335bfb5a8cb20ccae227b17bf59
Don Davis <dcaa2005@hotmail.com>
2018-08-13 08:52:50 -0600

    updated getmusic

--- app/components/Itunes/itunes-service.js
+++ app/components/Itunes/itunes-service.js
@@ -4,32 +4,32 @@ import Song from "../../models/Song.js";
 class ItunesService {
 
   //DO NOT MODIFY
-  // getMusicByArtist(artist) {
-  //   var url = 'https://itunes.apple.com/search?term=' + artist;
-  //   //Casts each object to 
-  //   //@ts-ignore
-  //   return $.getJSON(url)
-  //     .then(res => res.results.map(s => new Song(s)))
-  //     .catch(err => console.log(err))
-  // }
 
-
-  //use at school
   getMusicByArtist(artist) {
-
-    var url1 = '//bcw-getter.herokuapp.com/?url=';
-    var url2 = 'https://itunes.apple.com/search?term=' + artist
-    var apiUrl = url1 + encodeURIComponent(url2);
+    var url = 'https://itunes.apple.com/search?term=' + artist;
     //Casts each object to 
     //@ts-ignore
-    return $.getJSON(apiUrl).then(function (response) {
-      //debugger
-      var songList = response.results.map(s => {
-        return new Song(s)
-      })
-      return songList;
-    })
+    return $.getJSON(url)
+      .then(res => res.results.map(s => new Song(s)))
+      .catch(err => console.log(err))
   }
+
+  //use at school
+  // getMusicByArtist(artist) {
+
+  //   var url1 = '//bcw-getter.herokuapp.com/?url=';
+  //   var url2 = 'https://itunes.apple.com/search?term=' + artist
+  //   var apiUrl = url1 + encodeURIComponent(url2);
+  //   //Casts each object to 
+  //   //@ts-ignore
+  //   return $.getJSON(apiUrl).then(function (response) {
+  //     //debugger
+  //     var songList = response.results.map(s => {
+  //       return new Song(s)
+  //     })
+  //     return songList;
+  //   })
+  // }
 
 }
 
